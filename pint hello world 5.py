8441f3918d1f29fa20347e437cf84bff3e3bf571
gotoyota18 <gotoyota18@gmail.com>
2014-07-26 13:33:39 -0400

    made sports triva

---
+++ pint hello world 5.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+favorite = input("what is yor favorite kyle basch paint sceam")
+ 
+print("you ansered " + favorite)
+
+#######################################
+
+yesorno = input(" do ypu like camry, yes or no?")
+
+if yesorno == "yes":
+    print("yes  you are awesome")
+elif yesorno == "no":
+    print("bad choice")
+else:
+    print("I don't understand")
+
+########################################
+
+yesorno = input (" are the clevlend cavelerl good")
+if yesorno == "yes": 
+    print(" your are great at basketball")
+elif yesorno == "no":
+    print ("your need to look it up")
+else:
+    print ("what")
+    
