6aed211ba5e1c666cd30b4df6302a259ee168fd9
gotoyota18 <gotoyota18@gmail.com>
2014-08-09 13:16:17 -0400

    i was awesome today

---
+++ ppppppioiiiiittthhhhony.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+name = input("what is your name")
+
+print(name)
+
+color = input("what is your favorite color")
+if not color == 'blue':
+    print ("something will be bad will hapen")
+    
