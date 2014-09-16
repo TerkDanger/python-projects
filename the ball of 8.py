fd25fbf0b83b09b5a76d2cbdf76de1265f1ef901
gotoyota18 <gotoyota18@gmail.com>
2014-09-15 20:53:32 -0700

    I DID MY SECOND BATTLE SHIP AND FOR GOT HOW TO DO THE TOP ROW OF NUMBER

---
+++ the ball of 8.py
@@ -0,0 +1,16 @@
+impot random
+
+print ('ask the 8ball')
+
+
+answers = [
+    'of coures',
+    'oh ya',
+    'with out a doutd',
+    ]
+while True
+    questions = input('>')
+    print ("ask a question:")
+    answers = random.choice(answers)
+    print (answers)
+    print()
